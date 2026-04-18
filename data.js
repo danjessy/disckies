276eea3951969bdb0f0c02f80a7d8c6b250c993f
danjessy <danjessy927@gmail.com>
2026-04-19 01:14:30 +0200

    Add tracks to existing vinyl records

--- data.js
+++ data.js
@@ -1,4 +1,23 @@
 const vinyles = [
+   /*{
+  id: 1,
+  title: "Utopia",
+  artist: "Travis Scott",
+  year: 2023,
+  genre: "Hip-Hop",
+
+  covers: [
+    "url1",
+    "url2",
+    "url3"
+  ],
+
+  tracks: [
+    { title: "HYAENA", duration: "3:42" },
+    { title: "THANK GOD", duration: "3:04" }
+  ]
+}*/
+   
     {
         id: 1,
         title: "Utopia",
@@ -6,6 +25,27 @@ const vinyles = [
         year: 2023,
         genre: "Hip-Hop",
         cover: "https://tse3.mm.bing.net/th/id/OIP.IsmKLQrQ61KJfA5bWrdMygHaFS?pid=ImgDet&w=202&h=143&c=7&dpr=1,5&o=7&rm=3",
+        tracks: [
+    { title: "HYAENA", duration: "3:42" },
+    { title: "THANK GOD", duration: "3:04" },
+    { title: "MODERN JAM", duration: "3:15" },
+    { title: "MY EYES", duration: "3:20" },
+    { title: "GOD'S COUNTRY", duration: "3:50" },
+    { title: "SIRENS", duration: "4:00" },
+    { title: "MELTDOWN", duration: "3:45" },
+    { title: "FE!N", duration: "3:10" },
+    { title: "Delresto (Echoes)", duration: "3:35" },
+    { title: "I KNOW ?", duration: "3:05" },
+    { title: "Topia-twins", duration: ""},
+    { title: "Circus Maximus", duration:""},
+    { title: "PARASAIL", duration: "3:30" },
+    { title: "Skitzo", duration:""},
+    { title: "Lost Forever", duration:""},
+    { title: "Looove", duration:""},
+    { title: "K-Pop", duration:""},
+    { title: "Telekinesis", duration: "3:25" },
+    { title: "Til Further Notice", duration:""},
+  ],
         description: "https://www.discogs.com/fr/release/29155921-Travis-Scott-Utopia"
     },
     {
@@ -14,7 +54,27 @@ const vinyles = [
         artist: "The Weeknd",
         year: 2016,
         genre: "R&B",
-        cover: "https://www.pacsun.com/dw/image/v2/AAJE_PRD/on/demandware.static/-/Sites-pacsun_storefront_catalog/default/dw68184d1a/product_images/0685601190614NEW_01_089.jpg?sw=900"
+        cover: "https://www.pacsun.com/dw/image/v2/AAJE_PRD/on/demandware.static/-/Sites-pacsun_storefront_catalog/default/dw68184d1a/product_images/0685601190614NEW_01_089.jpg?sw=900",
+        tracks: [
+    { title: "STARBOY", duration: "" },
+    { title: "PARTY MONSTER", duration: "" },
+    { title: "FALSE ALARM", duration: "" },
+    { title: "REMINDER", duration: "" },
+    { title: "ROCKIN'", duration: "" },
+    { title: "SECRETS", duration: "" },
+    { title: "TRUE COLORS", duration: "" },
+    { title: "STARGIRL INTERLUDE", duration: "" },
+    { title: "SIDEWLAKS", duration: "" },
+    { title: "SIX FEET UNDER", duration: "" },
+    { title: "LOVE TO PLAY", duration: "" },
+    { title: "A LONELY NIGHT", duration: "" },
+    { title: "ATTENTION", duration: "" },
+    { title: "ORDINARY LIFE", duration: "" },
+    { title: "NOTHING WITHOUT YOU", duration: "" },
+    { title: "ALL I KNOW", duration: "" },
+    { title: "DIE FOR U", duration: "" },
+    { title: "I FEEL IT COMIN", duration: "" },
+  ]
     },
     {
         id: 3,
@@ -22,7 +82,19 @@ const vinyles = [
         artist: "Cigarettes After Sex",
         year: 2024,
         genre: ["Indie Pop","dream pop", "ambient pop", "shoegaze", "slowcore"],
-        cover: "https://musicalparadise.com/wp-content/uploads/2024/08/Jbkszw1fx3M4zddmL4pl0Gl6Uohcm4M0.png"
+        cover: "https://musicalparadise.com/wp-content/uploads/2024/08/Jbkszw1fx3M4zddmL4pl0Gl6Uohcm4M0.png",
+        tracks: [
+    { title: "X's", duration: "3:03" },
+    { title: "Tejano Blue", duration: "3:54" },
+    { title: "Silver Sable", duration: "3:51" },
+    { title: "Hideaway", duration: "4:36" },
+    { title: "Holding you, Holding me", duration: "3:30" },
+    { title: "Dark Vacay", duration: "3:33" },
+    { title: "Baby Blue Movie", duration: "4:04" },
+    { title: "Hot", duration: "3:57" },
+    { title: "Dreams From Bunker Hill", duration: "3:39" },
+    { title: "Ambien Slide", duration: "3:33" },
+        ]
     },
     {
         id: 4,
@@ -30,7 +102,19 @@ const vinyles = [
         artist: "Cigarettes After Sex",
         year: 2017,
         genre: ["Indie Pop","dream pop", "ambient pop", "shoegaze", "slowcore"],
-        cover: "https://th.bing.com/th/id/R.5857be624fa54ed7bdaf14c0a8125a79?rik=xudNi1RIFYi2vg&riu=http%3a%2f%2fthrills.cl%2fcdn%2fshop%2ffiles%2fCIGARRE.png%3fv%3d1725411298&ehk=J%2bYp4lNM1JhT0PQbfVtYw6AqYE1jOnHalMEwlq83BS0%3d&risl=&pid=ImgRaw&r=0"
+        cover: "https://th.bing.com/th/id/R.5857be624fa54ed7bdaf14c0a8125a79?rik=xudNi1RIFYi2vg&riu=http%3a%2f%2fthrills.cl%2fcdn%2fshop%2ffiles%2fCIGARRE.png%3fv%3d1725411298&ehk=J%2bYp4lNM1JhT0PQbfVtYw6AqYE1jOnHalMEwlq83BS0%3d&risl=&pid=ImgRaw&r=0",
+        tracks: [
+            { title: "K.", duration: "5:19" },
+            { title: "Each Time You Fall In Love", duration: "4:50" },
+            { title: "Sunsetz", duration: "3:34" },
+            { title: "Apocalypse", duration: "4:50" },
+            { title: "Flash", duration: "4:34" },
+            { title: "Sweet", duration: "4:51" },
+            { title: "Opera House", duration: "6:04" },
+            { title: "Truly", duration: "4:03" },
+            { title: "John Wayne", duration: "4:18" },
+            { title: "Young & Dumb", duration: "4:33" },
+        ]
     },
     {
         id: 5,
@@ -39,22 +123,50 @@ const vinyles = [
         year: 2019,
         genre: ["Indie Pop","dream pop", "ambient pop", "shoegaze", "slowcore"],
         cover: "https://www.therecordhub.com/cdn/shop/products/CIGARETTESCRY_1200x.png?v=1613125896",
+        tracks: [
+            { title: "Don't Let Me Go", duration: "4:22" },
+            { title: "Kiss It Off Me", duration: "4:29" },
+            { title: "Heavenly", duration: "4:48" },
+            { title: "You're the Only Good Thing in My Life", duration: "4:35" },
+            { title: "Touch", duration: "4:52" },
+            { title: "Hentai", duration: "4:45" },
+            { title: "Cry", duration: "4:16" },
+            { title: "Falling in Love", duration: "4:05" },
+            { title: "Pure", duration: "4:14" },
+        ]
+
     },
     {
         id: 6,
         title: "3 Symphonies",
-        artist: ["Mozart", "Schubert","Beethoven"],
+        artist: ["W.A. Mozart", "F. Schubert","L.V. Beethoven"],
         year: "XIXème siècle",
         genre: "Classique",
-        cover: "https://i.discogs.com/AzELiepxECaZbdHQowaGeYBFqxF-g7DECcO9En2Raj0/rs:fit/g:sm/q:90/h:516/w:550/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwMDQy/ODItMTM3NTIxODM4/Mi0yNTIzLmpwZWc.jpeg"
+        cover: "https://i.discogs.com/AzELiepxECaZbdHQowaGeYBFqxF-g7DECcO9En2Raj0/rs:fit/g:sm/q:90/h:516/w:550/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwMDQy/ODItMTM3NTIxODM4/Mi0yNTIzLmpwZWc.jpeg",
+        tracks: [
+            { title: "Symphonie n°5 en UT Mineur", duration: "" },
+            { title: "Symphonie n°8 en Si Mineur 'Inachevée'", duration: "" },
+            { title: "Symphonie n°32 en Sol Majeur, K.318", duration: "" },
+        ]
     },
     {
         id: 7,
         title: "Compilation classique I",
-        artist: ["Liszt", "Chopin"],
+        artist: ["F. Liszt", "F. Chopin"],
         year: "XIXème siècle",
         genre: "Classique",
-        cover: "https://img.leboncoin.fr/api/v1/lbcpb1/images/1b/dc/51/1bdc513e51ba4cf81f0e75efdf37667e002e395d.jpg?rule=classified-1200x800-webp"
+        cover: "https://img.leboncoin.fr/api/v1/lbcpb1/images/1b/dc/51/1bdc513e51ba4cf81f0e75efdf37667e002e395d.jpg?rule=classified-1200x800-webp",
+        tracks: [
+            { title: "Sonate en Si Mineur", duration: "29:54" },
+            { title: "Etude op.25 n°10", duration: "3:45" },
+            { title: "Etude op.25 n°12", duration: "2:35" },
+            { title: "3e Impromptu, op.51", duration: "5:39" },
+            { title: "Barcolle op.60", duration: "8:09" },
+            { title: "Mazurka op.17 n°1", duration: "2:17" },
+            { title: "Mazurka op.17 n°2", duration: "1:50" },
+            { title: "Mazurka op.17 n°4", duration: "4:15" },
+        ]
+
     },
     {
         id: 8,
@@ -62,47 +174,79 @@ const vinyles = [
         artist: "Alec R.Costandinos",
         year: "1977",
         genre: [""],
-        cover: "https://i.ebayimg.com/images/g/uyIAAOSwmg1jOPer/s-l1600.webp"
+        cover: "https://i.ebayimg.com/images/g/uyIAAOSwmg1jOPer/s-l1600.webp",
+        tracks: [
+            { title: "Accidental Lover", duration: "" },
+            { title: "I've Found Love 'Now I've Found You'", duration: "" },
+        ]
+
     },
     {
         id: 9,
         title: "Compilation classique II",
-        artist: ["Liszt", "Sarasate"],
+        artist: ["F. Liszt", "P. de Sarasate"],
         year: "Fin XIXème",
         genre: "Classique",
-        cover: "https://img.leboncoin.fr/api/v1/lbcpb1/images/31/32/01/3132016121ee782301ca20c1dd4709fe429a9778.jpg?rule=ad-large"
+        cover: "https://img.leboncoin.fr/api/v1/lbcpb1/images/31/32/01/3132016121ee782301ca20c1dd4709fe429a9778.jpg?rule=ad-large",
+        tracks: [
+            { title: "Rhapsodie Hongroise n°2", duration: "" },
+            { title: "Airs Bohemiens", duration: "" },
+            { title: "Les Préludes", duration: "" },
+        ]
     },
     {
         id: 10,
         title: "Cantates BWV",
-        artist: "Bach",
+        artist: "J.S. Bach",
         year: "XVIII",
         genre: "Classique",
-        cover: "https://m.media-amazon.com/images/I/61Jn2JarSoL._SX522_.jpg"
+        cover: "https://m.media-amazon.com/images/I/61Jn2JarSoL._SX522_.jpg",
+        tracks: [
+            { title: "Cantate BWV 106 'Actus Tragicus'", duration: "" },
+            { title: "Cantate BWV 106 'Ach wie flüchtig, ach wie nichtig'", duration: "" },
+        ]
     },
     {
         id: 11,
-        title: "Symphonie n°6 'Pathétique'",
-        artist: "Tchaïkovski",
+        title: "Symphonie n°6 en Si Mineur'Pathétique'",
+        artist: "P.I. Tchaïkovski",
         year: "XIX - XX",
         genre: "Classique",
-        cover: "https://i.ebayimg.com/images/g/eiQAAOSwJQdXCJbv/s-l1600.webp"
+        cover: "https://i.ebayimg.com/images/g/eiQAAOSwJQdXCJbv/s-l1600.webp",
+        tracks: [
+            { title: "Adagio - Allegro no troppo", duration: "" },
+            { title: "Alegro con grazia", duration: "" },
+            { title: "Allegro molto vivace", duration: "" },
+            { title: "Finale : Adagio lamentoso", duration: "" },
+        ]
     },
     {
         id: 12,
         title: "Les quatres saisons",
-        artist: "Vivaldi",
+        artist: "A. Vivaldi",
         year: "XVIII",
         genre: "Classique",
-        cover: "https://i.discogs.com/S8cXBzRDGQXwiD1DoR-eclCsagil_wJeR_3_AnRh4_s/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTg4MjY2/ODMtMTQ2OTYwNDIy/Ni00NzYxLmpwZWc.jpeg"
+        cover: "https://i.discogs.com/S8cXBzRDGQXwiD1DoR-eclCsagil_wJeR_3_AnRh4_s/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTg4MjY2/ODMtMTQ2OTYwNDIy/Ni00NzYxLmpwZWc.jpeg",
+        tracks: [
+            { title: "Le Printemps (La Primavera)", duration: "" },
+            { title: "L'été (L'Estate)", duration: "" },
+            { title: "L'Automne (L'Autounno)", duration: "" },
+            { title: "L'Hivers (L'Inverno)", duration: "" },
+        ]
     },
     {
         id: 13,
         title: "Symphonie Nr.5 c-moll op.67",
-        artist: "Beethoven",
+        artist: "L.V. Beethoven",
         year: "XVIII - XIX",
         genre: "Classique",
-        cover: "https://m.media-amazon.com/images/I/61X1gi1PnvL.jpg"
+        cover: "https://m.media-amazon.com/images/I/61X1gi1PnvL.jpg",
+        tracks:[
+            { title: "Satz: Allegro con brio", duration: "" },
+            { title: "Satz: Andante con moto", duration: "" },
+            { title: "Satz: Allegro - attacca", duration: "" },
+            { title: "Satz: Allegro", duration: "" },
+        ]
     },
     {
         id: 14,
@@ -110,7 +254,23 @@ const vinyles = [
         artist: "C. Aznavour",
         year: "XXème",
         genre:"",
-        cover: "https://m.media-amazon.com/images/I/51WjImeMPmL._SX522_.jpg"
+        cover: "https://m.media-amazon.com/images/I/51WjImeMPmL._SX522_.jpg",
+        tracks: [
+            { title: "Les Comédiens", duration: "2:21" },
+            { title: "J'aime Paris au Mois de Mai", duration: "2:34" },
+            { title: "Esperanza", duration: "2:34" },
+            { title: "Trousse Chemise", duration: "2:22" },
+            { title: "Terre Nouvelle", duration: "2:59" },
+            { title: "L'émigrant", duration: "2:52" },
+            { title: "Plus Bleu que Tes Yeux", duration: "3:00" },
+            { title: "Je M'Voyais Déjà", duration: "3:21" },
+            { title: "Jezebel", duration: "2:50" },
+            { title: "Je Hais Les Dimanches", duration: "2:55" },
+            { title: "Couchés Dans Le Foin", duration: "2:54" },
+            { title: "Mé Qué, Mé Qué", duration: "2:39" },
+            { title: "Alléluia", duration: "3:10" },
+            { title: "La Bagarre!...", duration: "3:12" },
+        ]
     },
     {
         id: 15,
@@ -118,6 +278,18 @@ const vinyles = [
         artist: "C. Aznavour",
         year: "XXème",
         genre:"",
-        cover: "https://m.media-amazon.com/images/I/91Sb0HCGvIL._AC_SX522_.jpg"
+        cover: "https://m.media-amazon.com/images/I/91Sb0HCGvIL._AC_SX522_.jpg",
+        tracks: [
+            { title: "La Bohème", duration: "4:03" },
+            { title: "Hier Encore", duration: "2:20" },
+            { title: "Les Comédiens", duration: "2:23" },
+            { title: "Emmenez-moi", duration: "3:31" },
+            { title: "For Me... Formidable", duration: "2:18" },
+            { title: "Je M'Voyais Déjà", duration: "3:22" },
+            { title: "La Mamma", duration: "3:43" },
+            { title: "Mes Emmerdes", duration: "3:04" },
+            { title: "Comme Ils Disent", duration: "4:53" },
+            { title: "Que C'est Triste Venise", duration: "2:36" },
+        ]
     }
 ]
